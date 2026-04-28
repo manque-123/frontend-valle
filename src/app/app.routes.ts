dfa58b5f477600433584459a9908a070dcecee95
SB- Alumno <SB-Alumno@duoc.acad>
2026-04-28 11:39:23 -0400

    Frontend Ionic funcionando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,18 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./modules/usuarios/login/login.page')
+      .then(m => m.LoginPage)
+  },
+  {
+    path: 'emergencias',
+    loadComponent: () => import('./pages/emergencias/emergencias.page')
+      .then(m => m.EmergenciasPage)
+  }
 ];
